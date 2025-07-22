163f8327c1a9da77d140bb6ecfa0beb0de688c6b
Arnab Deogharia <arnabdeo.2002@gmail.com>
2025-07-22 09:34:46 +0530

    src updated

---
+++ src/logger.py
@@ -0,0 +1,23 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+)
+
+#logger = logging.getLogger("ml_project_logger")
+#if __name__=="__main__":  (to test whether it works or not)
+#    logging.info("Logging has started")
+#logger = logging.getLogger(__name__)
+#print(logger)
